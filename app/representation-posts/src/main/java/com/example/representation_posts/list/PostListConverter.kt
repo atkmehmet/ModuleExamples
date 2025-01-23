dd1224f215da7205dcdc5a56beee84d28cf42f3b
atkmehmet <durmazmehmet47@gmail.com>
2025-01-24 00:11:25 +0300

    representation layer continue

--- app/representation-posts/src/main/java/com/example/representation_posts/list/PostListConverter.kt
+++ app/representation-posts/src/main/java/com/example/representation_posts/list/PostListConverter.kt
@@ -1,4 +1,28 @@
 package com.example.representation_posts.list
 
-class PostListConverter @Inject {
+import android.content.Context
+import com.example.mylibrary.UseCase.GetPostsWithUsersWithInteractionUseCase
+import com.example.mylibrary.entity.Result
+import com.example.representation_posts.R
+import dagger.hilt.android.qualifiers.ApplicationContext
+import javax.inject.Inject
+
+class PostListConverter @Inject constructor(
+    @ApplicationContext private val context: Context
+) {
+
+    fun convert(postListResult: Result<GetPostsWithUsersWithInteractionUseCase.Response>):UiState<PostListModel> {
+        return when (postListResult){
+            is Result.Error-> {
+                UiState.Error(postListResult.exception.localizedMessage.orEmpty())
+            }
+            is Result.Succes->{
+                UiState.Success(PostListModel(
+                    headerText = context.getString(
+
+                    )
+                ))
+            }
+        }
+    }
 }
