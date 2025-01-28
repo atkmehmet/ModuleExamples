2f24e6bd4d461ab6d085eb09c42df3cb4da80c18
atkmehmet <durmazmehmet47@gmail.com>
2025-01-28 23:37:29 +0300

    representation layer continue

--- app/src/main/java/com/example/moduleexamples/MainActivity.kt
+++ app/src/main/java/com/example/moduleexamples/MainActivity.kt
@@ -6,42 +6,43 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavController
+import androidx.navigation.NavHost
+import androidx.navigation.compose.rememberNavController
 import com.example.moduleexamples.ui.theme.ModuleExamplesTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
-            ModuleExamplesTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+            CleanAppTheme{
+                Surface(
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    val navController = rememberNavController()
+                    App(navController = navController)
+
                 }
+            }
             }
         }
     }
-}
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ModuleExamplesTheme {
-        Greeting("Android")
+fun App(navController: NavController){
+    NavHost(navController,startDestination ="/posts"){
+        composable(route = "/posts"){
+            PostListScreen(hiltViewModel())
+        }
     }
 }
